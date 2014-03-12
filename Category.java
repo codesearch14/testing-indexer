e6537561c93482a36a1d0ee25c998343d73f8d39
codesearch14 <jclspoti@gmail.com>
2014-03-12 23:43:24 +0100

    Create Category.java

---
+++ Category.java
@@ -0,0 +1,31 @@
+package org.model.category;
+
+
+public class Category {
+	private Long categoryId;
+	private String categoryName;
+
+	public Category(){
+		
+	}
+	
+	public Category(String categoryName){
+		this.categoryName=categoryName;
+	}
+	
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+}
