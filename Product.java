a483284e2055306a006b8e61dee49d62f06d24e0
codesearch14 <jclspoti@gmail.com>
2014-03-12 23:39:54 +0100

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,69 @@
+package org.model.product;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+import org.hibernate.annotations.BatchSize;
+
+import org.category.Category;
+
+@Entity
+@BatchSize(size = 20)
+public class Product {
+	private Long productId;
+	private String productName;
+	private Double price;
+	private Category category;
+	
+	public Product(){
+		
+	}
+	
+	public Product(String productName, Double price, Category category){
+		this.productName = productName;
+		this.price = price;
+		this.category = category;
+	}
+	
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	@ManyToOne(optional=false, fetch=FetchType.LAZY)
+    @JoinColumn(name="categoryId")
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+}
